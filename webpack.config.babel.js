b71d3adc7aca23654fe2d556cf68b37d6bcc72b3
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2017-04-22 04:08:46 +0900

    delete unnecessary configs in webpack.config.js

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,18 +2,14 @@ import webpack from 'webpack';
 import path from 'path';
 import GasPlugin from 'gas-webpack-plugin';
 
-const PRODUCTION = process.env.NODE_ENV === 'production';
-
 export default {
   context: path.resolve(__dirname, './lib'),
   entry: './index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, './src'),
-    publicPath: '/assets',
   },
 
-  devtool: PRODUCTION ? 'cheap-module-source-map' : 'inline-source-map',
   module: {
     loaders: [
       {
@@ -25,25 +21,5 @@ export default {
   },
   plugins: [
     new GasPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-      },
-    }),
-    ...(
-      PRODUCTION ? [
-        new webpack.optimize.UglifyJsPlugin({
-          compress: {
-            warnings: false,
-          },
-        }),
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.optimize.DedupePlugin(),
-      ] : []
-    ),
   ],
-  // http://webpack.github.io/docs/configuration.html#resolve-extensions
-  //resolve: {
-    //extensions: ['', '.js', '.jsx']
-  //}
 };
